5d8ad51071ede099fe6dfa400b4e5f0a12ad914e
beyamor <beyamor@gmail.com>
2014-02-16 17:16:50 -0800

    Started adding/removing things from grid

--- src/lapwing/entities.clj
+++ src/lapwing/entities.clj
@@ -1,10 +1,12 @@
 (ns lapwing.entities
   (:require [lapwing.entity :as entity]
-            [lapwing.util :as util])
+            [lapwing.util :as util]
+            [lonocloud.synthread :as ->])
   (:refer-clojure :exclude [filter remove]))
 
 (def empty-entities
-  {:entities {}})
+  {:entities  {}
+   :grid      {}})
 
 (declare add)
 
@@ -46,15 +48,55 @@
   (for [[id e] (:entities es)]
     (f e)))
 
+(def grid-size 200)
+
+(defn grid-dims
+  [e]
+  [(-> e entity/left (/ grid-size) Math/floor)
+   (-> e entity/right (/ grid-size) Math/ceil)
+   (-> e entity/top (/ grid-size) Math/floor)
+   (-> e entity/bottom (/ grid-size) Math/ceil)])
+
+(defn grid-indices
+  [e]
+  (let [[left right top bottom] (grid-dims e)]
+    (for [x (range left (inc right))
+          y (range top (inc bottom))]
+      [x y])))
+
+(defn add-to-grid
+  [grid e]
+  (let [id (entity/id e)]
+    (reduce
+      (fn [grid [x y]]
+        (update-in grid [x y] (fnil conj #{}) id))
+      grid (grid-indices e))))
+
+(defn remove-from-grid
+  [grid e]
+  (let [id (entity/id e)]
+    (reduce
+      (fn [grid [x y]]
+        (update-in grid [x y] (fnil disj #{}) id))
+      grid (grid-indices e))))
+
 (defn update-only
   [es who updater]
-  (let [id (entity/id who)]
-    (update-in es [:entities id] updater)))
+  (let [id        (entity/id who)
+        original  (get-in es [:entities id])
+        updated   (updater original)]
+    (-> es
+      (assoc-in [:entities id] updated)
+      (->/when (not= (:pos original) (:pos updated))
+               (->/in [:grid]
+                      (remove-from-grid original)
+                      (add-to-grid updated))))))
 
 (defn add
   [es e]
   (-> es
-    (assoc-in [:entities (entity/id e)] e)))
+    (assoc-in [:entities (entity/id e)] e)
+    (update-in [:grid] add-to-grid e)))
 
 (defn remove
   [es e]
